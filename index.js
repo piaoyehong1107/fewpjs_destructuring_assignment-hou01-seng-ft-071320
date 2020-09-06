1b17f234cfef6a03906ec8348edfe0647e5908c7
Yehong Piao <piaoyehong1107@gmail.com>
2020-09-06 04:23:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ const [moo,neigh,baa,oink,cluck]='cow horse sheep pig chicken'.split(' ')
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them Bessie, Dolly, Babe, and Little.
 
+
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
 
 // Arrays
